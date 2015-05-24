0d5accfa6d274ae742948482f1b42ccbb54b3cbb
Thomas Cool <thomas.cool@me.com>
2015-05-24 17:10:26 -0500

    more shadow work

--- Serenity.m
+++ Serenity.m
@@ -34,32 +34,100 @@ classdef Serenity < Shadow
           [isCast,CDLeft]=ApplyInstantCast(obj,obj.abilities.ds); 
       end
       function [isCast,CDLeft]=UseVanquish(obj)
-          if(false )%instantproc check here,apply vq dot in callback
+          if(obj.procs.FoS.LastProc>0 &&...
+             obj.procs.FoS.LastProc+obj.procs.FoS.Dur>obj.nextCast )%instantproc check here,apply vq dot in callback
             [isCast,CDLeft]=ApplyInstantCast(obj,obj.abilities.vq);
           else 
-            [isCast,CDLeft]=ApplyCastAbility(obj,obj.abilities.vq); 
+            [isCast,CDLeft]=ApplyCastAbilities(obj,obj.abilities.vq); 
           end
           
       end
       function [isCast,CDLeft]=UseForceInBalance(obj)
-           [isCast,CDLeft]=ApplyInstantCast(obj.abilities.fib);
+           [isCast,CDLeft]=obj.ApplyInstantCast(obj.abilities.fib);
       end
       function [isCast,CDLeft]=UseSeverForce(obj)
-           [isCast,CDLeft]=ApplyDot(obj.abilities.sf);
+           [isCast,CDLeft]=ApplyDot(obj,'SF',obj.abilities.sf);
       end
       function [isCast,CDLeft]=UseForceBreach(obj)
-          [isCast,CDLeft]=ApplyDot(obj.abilities.fb);
+          [isCast,CDLeft]=ApplyDot(obj,'FB',obj.abilities.fb);
       end
       function [isCast,CDLeft]=UseSerenityStrike(obj)
-          [isCast,CDLeft]=ApplyInstantCast(obj.abilities.ses);
+          [isCast,CDLeft]=obj.ApplyInstantCast(obj.abilities.ses);
       end
       
       function VQCallback(obj,~,~)
-         ApplyDot(obj,obj.abilities.vqd,1); 
+         ApplyDot(obj,'VQ',obj.abilities.vqd,1); 
       end
+      function FIBCallback(obj,t,~)
+          obj.buffs.FiB.LastUsed=t;
+          obj.buffs.FiB.Charges=15;
+      end
+      function DOTCheckCB(obj,t,~)
+          if(obj.procs.CS.Available < t)
+             if(rand() < 0.3)
+                 obj.procs.CS.Available=t+obj.procs.CS.CD*(1-obj.stats.Alacrity)*0.99;
+                 obj.procs.CS.LastProc=t;
+                 obj.avail.sps=t;
+             end
+          end
+      end
+      function [bd, bc, bs, baac,bmult]=CalculateBonus(obj,t,it)
+          bd=0;bc=0;bs=0;baac=0;bmult=1;
+           if(it.w==1 && obj.procs.FS.LastProc+obj.procs.FS.Dur>t)  %Force Synergy Bonus Dmg
+              bc=bc+0.05; 
+           end
+           n=it.id;
+           if(it.w==0 && obj.buffs.FiB.LastUsed>=0 && obj.buffs.FiB.Charges>0 &&...
+              obj.buffs.FiB.Dur+obj.buffs.FiB.LastUsed>t)
+                if(strcmp(it.id,'vanquishdot')||...
+                   strcmp(it.id,'forcebreach')||...
+                   strcmp(it.id,'severforce'))
+                    bmult=1.1;
+                    obj.buffs.FiB.Charges=obj.buffs.FiB.Charges-1;
+                end
+           end
+           
+
+      end
+      function AddDamage(obj,dmg,it)
+          t=dmg{1};
+          if(it.w==1 && dmg{5}==1)
+              if(rand()<0.5)
+                %Force Technique
+                [mhd,mhh,mhc]=obj.CalculateDamage(t,obj.abilities.ft);
+                AddDamage(obj,{t,obj.abilities.ft.name,mhd,mhc,mhh},it);
+              end
+              fos=obj.procs.FoS;
+              if(t>=fos.Available);
+                  fos.LastProc=t;
+                  fos.Available=t+fos.CD*(1-obj.stats.Alacrity)*0.99;
+                  obj.procs.FoS=fos;
+                  obj.avail.vanquish=t;
+              end
+          end
+          if(it.w==0 && dmg{5}==1 && dmg{4}==1)
+              %Proc Force Synergy
+              obj.procs.FS.LastProc=t;
+          end
+          if(it.dmg_type==3 )
+          elseif(it.dmg_type==1||it.dmg_type==2)
+              dmg{3}=dmg{3}*(1-CalculateBossDR(obj,it));
+          end
+          if(obj.total_damage<obj.total_HP)
+              %if(true)
+              if(dmg{4}>0)
+                  obj.crits=obj.crits+1;
+              end
+              obj.dmg_effects=obj.dmg_effects+1;
+              obj.total_damage=obj.total_damage+dmg{3};
+              obj.damage{end+1}=dmg;
+              AddToStats(obj,dmg);
+          end
+      end
+        
+              
 %Crush Spirit check goes into the dot check
-%Force Synergy check goes into add damage
-%Add Force Technique to the Add Damage
+
       
     end
     
--- Shadow.m
+++ Shadow.m
@@ -6,6 +6,98 @@ classdef Shadow < BaseSimulator
     end
     
     methods
+        function [bd, bc, bs, baac,bmult]=CalculateBonus(obj,t,it)
+           %No Need to calculate adrenals or relics here
+           bd=0;bc=0;bs=0;baac=0;bmult=0;
+        end
+        
+        function [mhd,mhh,mhc,ohd,ohh,ohc] = CalculateDamage(obj,t,it,autocrit)
+            if(nargin<4)
+                autocrit = false;
+            end
+            [bd_, bc_,bs_,baac_,bm_]=CalculateBonus(obj,t,it);
+            
+            if(t>obj.procs.SA.LastProc+obj.procs.SA.CD)
+                if(rand()<0.3)
+                    obj.SAprocs=obj.SAprocs+1;
+                    obj.procs.SA.LastProc=t;
+                end
+            end
+            if(t>obj.procs.FR.LastProc+obj.procs.FR.CD)
+                if(rand()<0.3)
+                    obj.FRprocs=obj.FRprocs+1;
+                    obj.procs.FR.LastProc=t;
+                end
+            end
+            bonusdmg=0+bd_;
+            bacc=0+baac_;
+            bc=0+bc_;    %Bonus Crit
+            bs=0+bs_;   %Bonus Surge
+            bm=0+bm_;   %Bonus Mult
+            %is Focused Retribution Procced
+            if(obj.procs.FR.LastProc>=0 && obj.procs.FR.LastProc+6>t)
+                bonusdmg = bonusdmg + obj.stats.FR_proc*0.2*1.05*1.05;
+            end
+            
+            %is Serendipidous Procced
+            if(obj.procs.SA.LastProc>=0 && obj.procs.SA.LastProc+6>t)
+                bonusdmg = bonusdmg + obj.stats.SA_proc*0.23*1.05;
+            end
+            
+            %is Adrenal Used
+            if(obj.buffs.AD.LastUsed>=0 && obj.buffs.AD.LastUsed+15>t)
+                bonusdmg = bonusdmg + obj.stats.adrenal_amt*0.23*1.05;
+            end
+
+            s_=obj.stats;
+            if(it.w==1)
+                rbonus = bonusdmg+obj.stats.RangedBonus;
+                mhm= (rbonus*it.c+...
+                    s_.MinMH*(1+it.Am)+it.Sm*it.Sh)*it.mult;
+                mhx= (rbonus*it.c+...
+                    s_.MaxMH*(1+it.Am)+it.Sx*it.Sh)*it.mult;
+                ohn= (s_.MinOH*(1+it.Am))*it.mult;
+                ohx= (s_.MaxOH*(1+it.Am))*it.mult;
+                
+                ohc = max(autocrit,rand()<(obj.stats.CritChance+bc+it.cb));
+                ohh = rand()<(it.base_acc-obj.boss_def+obj.stats.Accuracy-0.3+bacc);
+                ohd = (rand()*(ohx-ohn)+ohn)*(1+(s_.Surge+bs+it.sb)*ohc)*ohh*bm;
+                if(ohn==0 || ohx==0)
+                    ohd=-1;
+                end
+                %fprintf('%f %f %f\n',mhm,mhx,rbonus);
+            else
+                tbonus = bonusdmg+obj.stats.TechBonus;
+                mhm=(tbonus*it.c+it.Sm*it.Sh)*it.mult;
+                mhx=(tbonus*it.c+it.Sx*it.Sh)*it.mult;
+                ohc=0; ohh=0; ohd=-1;
+            end
+            mhc = max(rand()<(obj.stats.CritChance+it.cb+bc),autocrit);
+            %mhc=0;
+            mhh = rand()<(obj.stats.Accuracy+it.base_acc-obj.boss_def+bacc);
+            mhd = (rand()*(mhx-mhm)+mhm)*(1+(s_.Surge+it.sb)*mhc)*mhh*bm;
+            if(isfield(it,'charges'))
+                mhd=mhd*it.charges;
+            end
+            %fprintf('%f %f %f\n',mhm,mhx,mhd);
+            %2PC set bonus
+            if(t<obj.procs.ST.LastProc+15 && obj.procs.ST.LastProc>=0)
+                mhd=mhd*1.02;
+                ohd=ohd*1.02;
+            end;
+            %Apply Raid multipliers
+            mhd=mhd*it.raid_mult;
+            ohd=ohd*it.raid_mult;
+            %Sub 30% damage multiplier
+            if(obj.total_damage>obj.total_HP*0.7)
+                mhd=mhd*(1+it.s30);
+                ohd=ohd*(1+it.s30);
+            end
+            
+            
+        end
+        
+        
     end
     
 end
